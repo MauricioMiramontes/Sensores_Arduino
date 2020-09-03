61649f0f9b969bb65894727988bd773636e32a9b
MauricioMiramontes <mmiramontesr@gmail.com>
2020-09-03 18:15:43 -0500

    Se agrega alarma

--- Sensores.ino
+++ Sensores.ino
@@ -3,41 +3,63 @@
 int PIR = 2;
 int RELE = 3;
 int Puerta = 5;
-int isOpen = 0;
+bool isOpen = false;
 int isMoving = 0;
 unsigned long Time = 0;
 //Variable para checar cuanto tiempo lleva corriendo
 unsigned long TiempoTranscurrido = 0;
+unsigned long TiempoTranscurrido2 = 0;
 
 void setup(){
-    pinMode(PIR, INPUT);
-    pinMode(RELE, OUTPUT);
-  	pinMode(Puerta, INPUT);
+  Serial.begin(9600);
+  pinMode(PIR, INPUT);
+  pinMode(RELE, OUTPUT);
+  pinMode(Puerta, INPUT);
 }
 
 void MostrarTiempo(unsigned long seg){
-	Serial.println(seg);
-  	//Aqui pondremos la mierda quee va al display
+  Serial.println(seg);
+  //Aqui pondremos la mierda quee va al display
 }
 
 void loop(){
-    isOpen = digitalRead(Puerta); //Sensor puerta
-  	isMoving =digitalRead(PIR);   //Sensor Movimiento
-    if (isOpen == HIGH){
-      	Serial.println("Puerta abierta");
-      	MostrarTiempo((Time - TiempoTranscurrido));
-      	Time = millis(); //Tiempo Transcurrido deja de actualizarse
-    	if((Time - TiempoTranscurrido) >= 7000){
-      	 	digitalWrite(RELE, HIGH);
-          	if (((Time - TiempoTranscurrido) >= 10000) && ( isMoving == HIGH){
-              	
-            	 Serial.println("Suena Alarma");           	          
-            }	
-     	}
+  TiempoTranscurrido = millis();
+
+  if (digitalRead(Puerta) == HIGH){  //Sensor puerta
+    isOpen = true;
+    Serial.println("Puerta abierta");
+  } 
+  else{
+    
+    digitalWrite(RELE, LOW);
+    //Se actualiza el tiempo si la puerta  no esta abierta
+
+    isOpen = false;
+  }
+
+  while (isOpen){
+    Time = millis(); //Tiempo Transcurrido deja de actualizarse
+    MostrarTiempo((Time - TiempoTranscurrido));
+    if((Time - TiempoTranscurrido) >= 7000){
+
+      digitalWrite(RELE, HIGH);
+      if ((Time - TiempoTranscurrido) >= 10000) {
+        isMoving = digitalRead(PIR);   //Sensor Movimiento
+        if(isMoving == HIGH){
+          Serial.println("Se detecto movimiento");
+          TiempoTranscurrido2 = Time;
+          while((Time - TiempoTranscurrido2) <= 2000){
+            Serial.println("Sonando alarma");
+            Time = millis();
+          }
+          digitalWrite(RELE, LOW);
+          break;
+        }
+        else{
+          break;
+        }
+      }	
     }
-    else{
-        digitalWrite(RELE, LOW);
-      	//Se actualiza el tiempo si la puerta  no esta abierta
-      	TiempoTranscurrido = millis();	
-    }
+  }
 }
+
