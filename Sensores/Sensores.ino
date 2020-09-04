addc815ec9ba242e497d3a9fb358c93b597aa0a7
MauricioMiramontes <mmiramontesr@gmail.com>
2020-09-04 18:09:55 -0500

    Se limpia el codigo y se agrega MostrarTiempo()

--- Sensores/Sensores.ino
+++ Sensores/Sensores.ino
@@ -1,69 +1,93 @@
-//ToDo: Cambiar if por while
-//ToDo: Implementar MostrarTiempo()
-int PIR = 2;
-int RELE = 3;
-int Puerta = 5;
-bool isOpen = false;
-int isMoving = 0;
-int Alarma = 11;
-unsigned long Time = 0;
-//Variable para checar cuanto tiempo lleva corriendo
-unsigned long TiempoTranscurrido = 0;
-unsigned long TiempoTranscurrido2 = 0;
+//Sensores
+int s_Mov = 2;
+int s_Puerta = 5;
+//Variables de sensores
+bool p_abierta = false;
+int hay_mov = 0;
+//Salidas al RELE
+int RELE_Alarma = 7;
+int RELE_Led = 3;
+//Variables de referencia al tiempo 
+unsigned long Tiempo_Transcurrido = 0;
+unsigned long T_Refer_Ciclo = 0;
+unsigned long T_Refer_Alarma = 0;
 
-void setup(){
-  Serial.begin(9600);
-  pinMode(PIR, INPUT);
-  pinMode(RELE, OUTPUT);
-  pinMode(Puerta, INPUT);
-  pinMode (Alarma, OUTPUT);
+void setup()
+{
+  Serial.begin(9600); //Esta linea y todos los "Serial" se quitaran cuando ya no se hagan pruebas
+  //Entradas
+  pinMode(s_Mov, INPUT);
+  pinMode(s_Puerta, INPUT);
+  //Salidas
+  pinMode(RELE_Alarma, OUTPUT);
+  pinMode(RELE_Led, OUTPUT);
 }
 
-void MostrarTiempo(unsigned long seg){
-  Serial.println(seg);
-  //Aqui pondremos la mierda quee va al display
+void MostrarTiempo(unsigned long miliseg) //Muestra el tiempo del ciclo 
+{
+  float m, s;
+  unsigned long terminado;
+  m = int(miliseg / 60000);
+  terminado = miliseg % 60000;
+  s = int(terminado / 1000);
+
+  //Este bloque se quitara cuando ya no se necesiten pruebas
+  //-------------------------------------
+  Serial.print("Tiempo transcurrido: ");
+  Serial.print(m, 0);
+  Serial.print("m ");
+  Serial.print(s, 0);
+  Serial.println("s ");
+  //-------------------------------------
 }
 
-void loop(){
-  TiempoTranscurrido = millis();
+void loop()
+{
+  T_Refer_Ciclo = millis(); //Se inicia el tiempo de referencia para cuando inicie el ciclo 
 
-  if (digitalRead(Puerta) == HIGH){  //Sensor puerta
-    isOpen = true;
-    Serial.println("Puerta abierta");
-  } 
-  else{
-    
-    digitalWrite(RELE, LOW);
-    //Se actualiza el tiempo si la puerta  no esta abierta
-
-    isOpen = false;
+  if (digitalRead(s_Puerta) == HIGH) //Se revisa si la puerta se ha abierto 
+  { 
+    p_abierta = true;
+    Serial.println("s_Puerta abierta"); 
+  }
+  else
+  {
+    digitalWrite(RELE_Led, LOW);
+    p_abierta = false;
   }
 
-  while (isOpen){
-    Time = millis(); //Tiempo Transcurrido deja de actualizarse
-    MostrarTiempo((Time - TiempoTranscurrido));
-    if((Time - TiempoTranscurrido) >= 7000){
+  while (p_abierta) // Se inicia el ciclo 
+  {
+    Tiempo_Transcurrido = millis(); //Tiempo Transcurrido empieza a actualizarce 
 
-      digitalWrite(RELE, HIGH);
-      if ((Time - TiempoTranscurrido) >= 10000) {
-        isMoving = digitalRead(PIR);   //Sensor Movimiento
-        if(isMoving == HIGH){
+    MostrarTiempo((Tiempo_Transcurrido - T_Refer_Ciclo));
+
+    if ((Tiempo_Transcurrido - T_Refer_Ciclo) >= 7000) //Si han pasado 7 minutos
+    {
+      digitalWrite(RELE_Led, HIGH);
+      if ((Tiempo_Transcurrido - T_Refer_Ciclo) >= 10000) //Si han pasado 10 minutos
+      {
+        hay_mov = digitalRead(s_Mov); //Sensor Movimiento
+        if (hay_mov == HIGH) //Si se detecta movimiento 
+        {
           Serial.println("Se detecto movimiento");
-          TiempoTranscurrido2 = Time;
-          while((Time - TiempoTranscurrido2) <= 2000){
-            Serial.println("Sonando alarma");
-             digitalWrite(Alarma, HIGH);
-             delay(200);
-             digitalWrite(Alarma, LOW);
-            Time = millis();
+          T_Refer_Alarma = Tiempo_Transcurrido; // Se inicia el tiempo de referencia para sonar la alarma 
+          digitalWrite(RELE_Alarma, HIGH); 
+          while ((Tiempo_Transcurrido - T_Refer_Alarma) <= 2000) //Mientras no haya pasado 1 minuto 
+          {
+            Serial.println("Sonando RELE_Alarma");
+            Tiempo_Transcurrido = millis();
           }
-          digitalWrite(RELE, LOW);
+          digitalWrite(RELE_Alarma, LOW); //Se apaga alarma y led antes de terminar el ciclo 
+          digitalWrite(RELE_Led, LOW);
           break;
         }
-        else{
+        else
+        {
+          digitalWrite(RELE_Led, LOW);
           break;
         }
-      }	
+      }
     }
   }
 }
